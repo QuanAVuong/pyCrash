d124bf5d3b76129ee528836bf0483c2b17053938
Quan <QuanAVuong@users.noreply.github.com>
2018-03-23 23:42:06 -0400

    add question 5

---
+++ 1.py
@@ -0,0 +1,31 @@
+# 1
+a = 2
+a = 4
+a = 6
+print(a + a + a) # => 18
+
+# 2
+a = 1
+_a = 2
+_a2 = 3
+2a = 4 # variables' names cannot start with a number
+
+# 3
+a = 1
+b = 2
+print(a == b) # => False
+print(b == c) # => NameError: name 'c' is not defined
+
+# 4.
+a = "1"
+b = 2
+# print(a + b) # TypeError: must be str, not int  
+print(int(a) + b) 
+
+# 5
+# Question: Complete the script so that it prints out the second item of the list.
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# b
