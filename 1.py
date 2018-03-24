7355394597d361af2e8b05afafe457bce4c916eb
Quan <QuanAVuong@users.noreply.github.com>
2018-03-24 00:25:27 -0400

    complete 13

--- 1.py
+++ 1.py
@@ -83,3 +83,13 @@ my_range = range(1, 21)
 # [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200] 
 print([10 * num for num in my_range ])
 
+
+# 13 Complete the script so it generates the expected output using my_range  as input data. Please note that the items of the expected list output are all strings.
+
+my_range = range(1, 21)
+#  Expected output: 
+
+# ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']  
+print([str(num) for num in my_range]) # list comprehension
+print(list(map(str, my_range))) # map function
+
