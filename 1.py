d6ed0374dd06d7953713124711088b1172173927
Quan <QuanAVuong@users.noreply.github.com>
2018-03-23 23:58:06 -0400

    complete 6

--- 1.py
+++ 1.py
@@ -30,3 +30,13 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 
 # b
 print(letters[1])
+
+
+#6
+# Question: Please complete the script so that it prints out a list slice containing items d , e , and f .
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# ['d', 'e', 'f'] 
+print(letters[3:6])
