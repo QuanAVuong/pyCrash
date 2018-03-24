e5d32d2e3b526c6b171012bdee560b013941aafe
Quan <QuanAVuong@users.noreply.github.com>
2018-03-23 23:52:32 -0400

    complete 5

--- 1.py
+++ 1.py
@@ -25,7 +25,8 @@ print(int(a) + b)
 # 5
 # Question: Complete the script so that it prints out the second item of the list.
 
-# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 # Expected output: 
 
 # b
+print(letters[1])
