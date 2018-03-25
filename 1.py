446c9b347d51d83c889ede34e7eaa399526973ab
Quan <QuanAVuong@users.noreply.github.com>
2018-03-24 20:46:04 -0400

    complete 17

--- 1.py
+++ 1.py
@@ -140,3 +140,13 @@ d = {"a": 1, "b": 2}
 
 print(d["b"])
 
+
+# 17 Calculate the sum of the values of keys a  and b .
+
+d = {"a": 1, "b": 2, "c": 3}
+# Expected output: 
+
+# 3  
+print(d["a"] + d["b"])
+
+
