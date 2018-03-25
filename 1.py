63486254a697319e05e6ec84cbc72d1e2b5a1fed
Quan <QuanAVuong@users.noreply.github.com>
2018-03-24 20:43:09 -0400

    complete 16

--- 1.py
+++ 1.py
@@ -131,3 +131,12 @@ d1 = {"a": 1, "b": 2}
 d2 = dict(a=1, b=2)
 print(d1, d2)
 
+
+# 16 Please complete the script so that it prints out the value of key b .
+
+d = {"a": 1, "b": 2}
+# Expected output: 
+# 2  
+
+print(d["b"])
+
