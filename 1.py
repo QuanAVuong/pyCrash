bd7454a10174c7f13017d2aae2d7813f965e9f0c
Quan <QuanAVuong@users.noreply.github.com>
2018-03-25 19:54:41 -0400

    complete 24

--- 1.py
+++ 1.py
@@ -209,3 +209,22 @@ d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)
 # 13  
 
 print(d["b"][2])
+
+
+# 24 Please complete the script so that it prints out the expected output.
+# Expected output: 
+# b has value [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# c has value [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+# a has value [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+from pprint import pprint
+d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+
+# for key in d:
+#     pprint(key + " has value " + str(d[key]))
+# pprint(d)
+
+# dict(print(key + " has value " + str(value)) for key, value in d.items()) # TypeError: cannot convert dictionary update sequence element #0 to a sequence
+{pprint(key + " has value " + str(value)) for key, value in d.items()} # with items()
+{print(key, " has value ", str(d[key])) for key in d} # without items(), "+""
+
