6f46c27ffd5050d1d2de82226b0cbf543e165e83
Quan <QuanAVuong@users.noreply.github.com>
2018-03-26 15:59:38 -0400

    complete 39

--- 2.py
+++ 2.py
@@ -137,3 +137,12 @@ import math # => needs to import module
 print(math.sqrt(9)) # NameError: name 'math' is not defined
 # Expected output:
 # 3 
+
+
+# 39 The script is supposed to output the cosine of angle 1 radian, but instead it is throwing an error. Please fix the code so that it prints out the expected output.
+
+import math
+# print(math.cosine(1)) # AttributeError: module 'math' has no attribute 'cosine' 
+print(math.cos(1)) 
+# Expected output:
+# 0.5403023058681397          
