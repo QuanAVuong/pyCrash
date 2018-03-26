be746b172b09a78b5a39da2845bf105f27542a5b
Quan <QuanAVuong@users.noreply.github.com>
2018-03-25 21:05:13 -0400

    complete 29

--- 1.py
+++ 1.py
@@ -272,3 +272,15 @@ def foo(a, b):
 x = foo(2, 3) * 10 # TypeError: unsupported operand type(s) for *: 'NoneType' and 'int'
 print(x)
 
+
+# 29 Please write a function that calculates liquid volume in a sphere using the following formula. The radius r  is always 10, so consider making it a default parameter. Liquid volume = ( (4*pi**3)/3 ) - ( pi*h**2*(3*r-h)/3 )
+
+# You can then test your solution by passing 2 for h and you should get the expected output.
+# Expected output:
+# 4071.5040790523717
+
+from math import pi
+def liquid_volume(h, r=10):
+    return (( 4 * pi * r**3 ) / 3) - (pi * h**2 * (3*r - h) / 3 )
+
+print(liquid_volume(2))
