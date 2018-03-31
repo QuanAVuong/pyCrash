734caabcc6769440fc024c17c1e97aac944fa329
Quan <QuanAVuong@users.noreply.github.com>
2018-03-30 21:17:26 -0400

    fix not-fixed untracked files 2

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-age = input("What's your age? ")
-# age_last_year = age - 1         # TypeError: unsupported operand type(s) for -: 'str' and 'int'
-age_last_year = int(age) - 1         
-print("Last year you were %s." % age_last_year)
