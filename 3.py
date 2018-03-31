da75de7be3cd3d03c042d508e0fdca32fd02a7e0
Quan <QuanAVuong@users.noreply.github.com>
2018-03-30 21:05:04 -0400

    fixed untracked files

---
+++ 3.py
@@ -0,0 +1,5 @@
+# 51 The code produces an error. Please understand the error and try to fix it
+
+# print(type("Hey".replace("ey","i")[-1])     # SyntaxError: unexpected EOF while parsing
+print(type("Hey".replace("ey","i")[-1]))
+
