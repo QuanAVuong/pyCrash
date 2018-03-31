106a7dd67ac82a2da3b03af6fc83616c5cef2a5e
Quan <QuanAVuong@users.noreply.github.com>
2018-03-31 16:06:48 -0400

    complete 55

--- 3.py
+++ 3.py
@@ -46,3 +46,22 @@ d = {"employees":[{"firstName": "John", "lastName": "Doe"},
 d["employees"][1]["lastName"] = "Smooth"
 print(d)
 
+
+# 55 Please add a new employee to the dictionary.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+# Expected output: 
+# {'employees': [{'firstName': 'John', 'lastName': 'Doe'},
+#                {'firstName': 'Anna', 'lastName': 'Smith'},
+#                {'firstName': 'Peter', 'lastName': 'Jones'},
+#                {'firstName': 'Albert', 'lastName': 'Bert'}],
+#  'owners': [{'firstName': 'Jack', 'lastName': 'Petter'},
+#             {'firstName': 'Jessy', 'lastName': 'Petter'}]}
+
+d["employees"].append({"firstName": "Albert", "lastName": "Bert"})
+print(d)
