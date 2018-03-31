435c4d68dcc589de4d6bcf3a22dd97cfb411b90e
Quan <QuanAVuong@users.noreply.github.com>
2018-03-30 20:24:14 -0400

    complete 49

--- 2.py
+++ 2.py
@@ -291,3 +291,10 @@ for letter in "Hello":
 # e 
 
 
+# 49 The code is supposed to get some input from the user, but instead it produces an error. Please try to understand the error and then fix it.
+
+pass = input("Please enter your password: ") # SyntaxError: invalid syntax => using reserve keyword as variable name
+# Note: Please use raw_input  instead of input if you are on Python 2. For Python 3 input  is fine.
+password = input("Please enter your password: ")
+
+
