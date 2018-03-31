dd7617b1b6894a34f95dc632e8515b965ca63e76
Quan <QuanAVuong@users.noreply.github.com>
2018-03-31 15:56:27 -0400

    complete 53

--- 3.py
+++ 3.py
@@ -14,3 +14,16 @@ secondname = input("Enter second name: ")
 # Your first name is John and your second name is Smith 
 print("Your first name is %s and your second name is %s" % (firstname, secondname)) # tuple expected after %
 
+
+# 53 Print out the last name of the second employee.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+# Expected output: 
+# Smith 
+
+print(d["employees"][1]["lastName"])
+
