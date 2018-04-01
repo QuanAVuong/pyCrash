25bd21f9f129cc8e252aba759a8c9e74f51d1953
Quan <QuanAVuong@users.noreply.github.com>
2018-03-31 20:42:34 -0400

    complete 62

--- 3.py
+++ 3.py
@@ -140,3 +140,23 @@ import time
 while True:
     print("Hello")
     time.sleep(2)
+
+
+# 62 Create a program that once executed the program prints Hello  instantly first, then it prints it after 1 second, then after 2, 3, 4, and so on the interval increases between prints.
+
+# Expected output: 
+# ...
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# ...
+import time
+timer = 0
+while True:
+    print("Hello")
+    timer += 1
+    print(timer)
+    time.sleep(timer)
