9b6ea97adbd4d95520268e6478f1c82b6c60e0e7
Quan <QuanAVuong@users.noreply.github.com>
2018-04-05 23:01:56 -0400

    complete math 2

--- math.py
+++ math.py
@@ -11,3 +11,15 @@ rocks = {
 
 [ print(f" There is {round( value / sum(rocks.values() ), 2)} probability of the {key} rock ")
     for key, value in rocks.items() ]
+
+
+# 2 X is a discrete random variable. The table below defines a probability distribution for X. What is the expected value of X?
+
+x = [10, 20, 30, 40]
+p = [.2, .3, .15, .35]
+e = 0
+
+for x, p in zip(x, p):
+    print(f"{x} * {p} = {x*p} + ")
+    e += x*p     
+print(f"Expected Value: {e}")
