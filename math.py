5de2b3b991ffc9a94248aa56ba82600b21ac70c9
Quan <QuanAVuong@users.noreply.github.com>
2018-04-08 16:51:26 -0400

    complete math 4

--- math.py
+++ math.py
@@ -38,3 +38,36 @@ variance = 0
 for rv, p in pdf.items():
     variance += (rv - e)**2 * p
 print(sqrt(variance))
+
+
+# 4 Calculate & Plot Normal Cumulative Distribution Function
+# print(norm.__doc__)
+# https://docs.scipy.org/doc/scipy/reference/tutorial/stats.html
+# https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.norm.html
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.rv_continuous.html#scipy.stats.rv_continuous
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.truncnorm.html
+from scipy.stats import truncnorm, norm
+import matplotlib.pyplot as plt
+
+def normalCdf(lower=truncnorm.a, upper=truncnorm.b, mean=0, sd=1):
+	X = truncnorm( (lower - mean) / sd,
+									(upper - mean) / sd,
+									loc=mean, scale=sd )
+
+	N = norm(loc=mean, scale=sd)
+	truncdf = N.cdf(upper) - N.cdf(lower)
+	print(truncdf)
+
+	fig, ax = plt.subplots(2, sharex=True)
+	ax[0].hist(N.rvs(1000000), normed=True)
+	ax[1].hist(X.rvs(1000000), normed=True)
+
+	xmin, xmax = plt.xlim()
+	ymin, ymax = plt.ylim()
+
+	plt.annotate(f"CDF= {truncdf}", xy=(upper, .5*ymax), xytext=(upper + (xmax - xmin)/4, .75*ymax),
+								arrowprops=dict(facecolor="red", shrink=0.05, connectionstyle="angle3") )
+
+	plt.show()
+
+normalCdf(-999999, 0, 8, 10)
